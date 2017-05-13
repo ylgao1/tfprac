0bd3f375068b6f4e43313fe404f59b34e1aeb6f2
michael <18alexanderm117@tongji.edu.cn>
2017-05-13 16:14:04 +0800

    finished c3t4

--- c3t4.py
+++ c3t4.py
@@ -21,8 +21,26 @@ dataset_size = 128
 X = np.random.rand(dataset_size, 2)
 Y = np.array([int(x1+x2<1) for (x1,x2) in X], dtype=np.float32)[None].T
 
-# with tf.Session()
+batch_size = 8
 
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    print(w1.eval(sess))
+    print(w2.eval(sess))
+
+    STEPS = 5000
+    for i in range(STEPS):
+        start = (i * batch_size) % dataset_size
+        end = min(start+batch_size, dataset_size)
+        sess.run(train_op, feed_dict={x:X[start:end], y_:Y[start:end]})
+        if i % 1000 == 0:
+            total_cross_entropy = sess.run(cross_entropy, feed_dict={x:X, y_:Y})
+            print(f'After {i} trainning step(s), cross entropy on all data is {total_cross_entropy:g}')
+
+
+    print(w1.eval(sess))
+    print(w2.eval(sess))
 
 
 
