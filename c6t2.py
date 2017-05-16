b457e643ea7e2b63d58034b5110622b57731b524
Michael Chuai <alexanderm117@hotmail.com>
2017-05-16 23:43:27 +0800

    c6

--- c6t2.py
+++ c6t2.py
@@ -22,4 +22,31 @@ def inference(input_tensor, train, regularizer):
                                         initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv1_biases = tf.get_variable('bias', [CONV1_DEEP],
                                        initializer=tf.constant_initializer(0.0))
-        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, )
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, [1,1,1,1], 'SAME')
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    with tf.variable_scope('layer2-pool1'):
+        pool1 = tf.nn.max_pool(relu1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+    with tf.variable_scope('layer3-conv2'):
+        conv2_weights = tf.get_variable('weight', [CONV2_SIZE, CONV2_SIZE, CONV1_DEEP, CONV2_DEEP],
+                                        initializer=tf.truncated_normal(stddev=0.1))
+        conv2_biases = tf.get_variable('bias', [CONV2_DEEP],
+                                       initializer=tf.constant_initializer(0.0))
+        conv2 = tf.nn.conv2d(pool1, conv2_weights, [1,1,1,1], 'SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+
+    with tf.variable_scope('layer4-pool2'):
+        pool2 = tf.nn.max_pool(relu2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+    pool_shape = pool2.get_shape().as_list()
+
+
+
+
+
+
+
+
+
+
