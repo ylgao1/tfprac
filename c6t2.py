9fb9a8e4e0358e983e0268f47e5841f3259916cf
michael <18alexanderm117@tongji.edu.cn>
2017-05-16 16:59:28 +0800

    begin chap6

---
+++ c6t2.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+NUM_CLASSES = 10
+
+CONV1_DEEP = 32
+CONV1_SIZE = 5
+
+CONV2_DEEP = 64
+CONV2_SIZE = 5
+
+FC_SIZE = 512
+
+
+def inference(input_tensor, train, regularizer):
+    with tf.variable_scope('layer1-conv1'):
+        conv1_weights = tf.get_variable('weight', [CONV1_SIZE, CONV1_SIZE, NUM_CHANNELS, CONV1_DEEP],
+                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_biases = tf.get_variable('bias', [CONV1_DEEP],
+                                       initializer=tf.constant_initializer(0.0))
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, )
