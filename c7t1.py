44a381845a65a3bccf213946bd294c3e02736198
michael <18alexanderm117@tongji.edu.cn>
2017-05-18 16:30:24 +0800

    c7

---
+++ c7t1.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+mnist = input_data.read_data_sets('temp/MNIST_data', one_hot=True, dtype=tf.uint8)
+
+images = mnist.train.images
+labels = mnist.train.labels
+pixels = images.shape[1]
+num_examples = mnist.train.num_examples
+
+filename = 'temp/c7/c7.tfrec'
+
+writer = tf.python_io.TFRecordWriter(filename)
+for idx in range(num_examples):
+    image_raw = images[idx].tostring()
